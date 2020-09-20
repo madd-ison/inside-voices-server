03e21110bad6e3533d90238fe01489f4d753b582
Maddison Murphy <maddison@puppyteeth.org>
2020-09-20 15:54:53 -0400

    knex, pg

--- src/app.js
+++ src/app.js
@@ -18,56 +18,60 @@ app.use(cors())
 app.get('/', (req, res) => {
    res.send('Boilerplate!')
  })
-
- app.get('/journal', (req, res) => {
-
- })
-
- app.post('/register', (req, res) => {
-   const {username, password, confirmPassword} = req.body
-   if (username.length < 6 || username.length > 20) {
-    return res
-      .status(400)
-      .send('Username must be between 6 and 20 characters');
-  }
-
-  if (password.length < 8 || password.length > 36) {
-    return res
-      .status(400)
-      .send('Password must be between 8 and 36 characters');
-  }
-
-  if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
-    return res
-      .status(400)
-      .send('Password must be contain at least one digit');
-  }
-
-  if (!password.match(confirmPassword)) {
-    return res
-      .status(400)
-      .send('Passwords do not match')
-  }
- })
-
- app.post('/login', (req, res) => {
-   const {username, password} = req.body
-
- })
-
- app.post('/journal', (req, res) => {
-  const {content} = req.body
-  if (!content) {
-    return res
-      .status(400)
-      .send('Cannot submit blank entry');
-  }
- })
-
- app.delete('/journal/:journalId', (req, res) => {
-  const {journalId} = req.params
-  res.send(`Journal ${journalId} deleted.`);
+ app.get('/api/*', (req, res) => {
+  res.json({ok: true});
 });
+
+app.get('/journal', (req, res) => {
+
+})
+
+app.post('/register', (req, res) => {
+  const {username, password, confirmPassword} = req.body
+  if (username.length < 6 || username.length > 20) {
+   return res
+     .status(400)
+     .send('Username must be between 6 and 20 characters');
+ }
+
+ if (password.length < 8 || password.length > 36) {
+   return res
+     .status(400)
+     .send('Password must be between 8 and 36 characters');
+ }
+
+ if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
+   return res
+     .status(400)
+     .send('Password must be contain at least one digit');
+ }
+
+ if (!password.match(confirmPassword)) {
+   return res
+     .status(400)
+     .send('Passwords do not match')
+ }
+})
+
+app.post('/login', (req, res) => {
+  const {username, password} = req.body
+
+})
+
+app.post('api/journal', (req, res) => {
+ const {content} = req.body
+ if (!content) {
+   return res
+     .status(400)
+     .send('Cannot submit blank entry');
+ }
+})
+
+app.delete('api/journal/:journalId', (req, res) => {
+ const {journalId} = req.params
+ res.send(`Journal ${journalId} deleted.`);
+});
+
 
 app.use(function errorHandler(error, req, res, next) {
     let response
